d516ee305c279f4d93deedc6defeab076e313840
admin <admin@admins-MacBook-Pro.local>
2019-09-12 12:40:07 -0700

    mvp complete day 2

--- src/App.js
+++ src/App.js
@@ -3,27 +3,37 @@ import POTD from './Components/POTD'
 import StretchPOTD from './Components/StretchComponents/StretchPOTD'
 import styled from 'styled-components'
 
-const App = styled.div`
-  margin: 0;
+const AppBody = styled.div`
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
     "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
     sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  width:100%;
+  margin:auto;
+  background-color:#011628;
+  display:flex;
+  justify-content:center;
+  flex-wrap:wrap;
+  background-image:radial-gradient(white 1%, transparent 4%),radial-gradient(white 1%, transparent 4%);
+  background-position: 0px 0px, 50px 20px;
+  background-size: 20px 25px;
 `
 const POTDTitle = styled.h1`
-  font-family: 'Cabin', sans-serif;
-  font-weight:600;
+  font-family: 'Aldrich', sans-serif;
+  font-size:4rem;
+  color:white;
+  margin-bottom:0;
 `
 
 function App() {
   return (
-    <App>
+    <AppBody>
       <POTDTitle>Astronomy Photo of the Day</POTDTitle>
         <POTD />
-      <POTDTitle>Stretch : Astronomy Photo of the Day</POTDTitle>
+      <POTDTitle>Bonus Photo of the Day</POTDTitle>
         <StretchPOTD />
-    </App>
+    </AppBody>
   ); 
 }
 
--- src/Components/POTDCard.js
+++ src/Components/POTDCard.js
@@ -1,29 +1,56 @@
 import React from 'react';
 import styled from 'styled-components'
 
-const POTDCard = props => {
-
-     const Container = styled.div`
+const Container = styled.div`
           font-family: 'Open Sans', sans-serif;
+          color:white;
+          max-width:1000px;
+          line-height:25px;
+          display:flex;
+          justify-content:space-between;
+          height:600px;
+          text-align:center;
+          align-items:center;
      `
-     const Picture = styled.img`
+const Picture = styled.img`
           width:100%;
      `
 
+const PictureContainer = styled.div`
+     max-width:60%;
+`
+
+const ContentContainer = styled.div`
+     max-width:38%;
+     display:flex;
+     justify-content:center;
+     flex-wrap:wrap;
+     height:400px;
+     overflow:scroll;
+     text-align:center;
+     padding:20px;
+`
+const ExplanationContainer = styled.div`
+     text-align:justify;
+`
+
+
+const POTDCard = props => {
      // props is an object passing another object
      // access props to info and then access items in the object
 console.log(props.info.url)
      return (
           <Container>
-               <div className="photo">
+               <ContentContainer>
+                    <h3>{props.info.title} <br /> {props.info.date} </h3>
+                    <ExplanationContainer>
+                         <p>{props.info.explanation}</p>
+                         <span><p>CopyRight: {props.info.copyright}</p></span>
+                    </ExplanationContainer>
+               </ContentContainer>
+               <PictureContainer>
                     <Picture src={props.info.hdurl} alt="NASA of the day" />
-               </div>
-               <div className="photoContent">
-                    <h3>{props.info.title}</h3>
-                    <p>{props.info.date}</p>
-                    <p>{props.info.explanation}</p>
-                    <span><p>CopyRight: {props.info.copyright}</p></span>
-               </div>
+               </PictureContainer>
           </Container>
      )
 }
